5768b724e222eeef7ced5e6e3e1a9cb5dc07136d
antonioaltamura <antonioaltamura7@gmail.com>
2017-06-28 00:44:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Antonio Altamura on 28/06/2017.
+ */
+"use strict";
+let express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser');
+
+let index = require('./routes/index'),
+	detail = require('./routes/detail');
+
+let app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/detail', detail);
+
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
