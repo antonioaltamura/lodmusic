b18c3893c17265446b5a582ccea585e8b425101d
Mario Villani <villani.mario90@gmail.com>
2017-07-03 15:15:14 +0200

    cleaning + autocomplete

--- routes/api.js
+++ routes/api.js
@@ -5,19 +5,10 @@
 let router = require('express').Router();
 let sparql = require('./sparql');
 
-
-var escape = function (str){ return String(str).replace(/([.*+?=^!:${}()|[\]\/\\])/g, '\\$1');}
-var startsWith = function(str) {
-	if(str)
-		return  new RegExp('^'+escape(str), "i");
-	else
-		return new RegExp('.*', "i");
-};
-
-router.get('/autocomplete/', function(req,res) {
-	if(!req.query.term)
-		res.status(400).send('No query param');
-	sparql.query(`SELECT DISTINCT ?name ?artist ?bDate ?abstract ?image
+router.get('/autocomplete/', function (req, res) {
+    if (!req.query.term)
+        res.status(400).send('No query param');
+    sparql.query(`SELECT DISTINCT ?name ?artist ?bDate ?abstract ?image
 WHERE
 {
   ?artist a umbelrc:MusicalPerformer;
@@ -26,71 +17,13 @@ WHERE
   dbo:birthDate ?bDate.
  OPTIONAL{?artist dbo:thumbnail ?image}.
  FILTER regex(?name, "^${req.query.term}","i").
-}`, function(r){
-		let json = JSON.parse(r);
-		let result = json.results.bindings.map(function(item){
-			return {name: item.name.value, uri: item.artist.value};
-		});
-		res.json(result);
-	})
-});
-
-router.get('/', function(req, res){
-
-	console.log(req.query.q);
-
-	let query = `
-				SELECT *
-				FROM <http://dbpedia.org>
-				WHERE {
-					?city <http://dbpedia.org/property/leaderName> ?leaderName
-				} LIMIT 10
-`;
-	let client = new SparqlClient(endpoint);
-	//console.log("Query to " + endpoint);
-	//console.log("Query: " + query);
-	client.query(query)
-	//.bind('city', 'db:Chicago')
-	//.bind('city', 'db:Tokyo')
-	//.bind('city', 'db:Casablanca')
-		.bind('city', '<http://dbpedia.org/resource/Vienna>')
-		.execute(function(error, results) {
-			return res.json(results);
-		});
-	});
-
-router.get('/memberState', function(req,res) {
-	var memberState=["Austria",
-		"Belgium",
-		"Bulgaria",
-		"Cyprus",
-		"Croatia",
-		"Czech Republic",
-		"Denmark",
-		"Estonia",
-		"Finland",
-		"France",
-		"Germany",
-		"Greece",
-		"Hungary",
-		"Ireland",
-		"Italy",
-		"Latvia",
-		"Lithuania",
-		"Luxembourg",
-		"Malta",
-		"Netherlands",
-		"Poland",
-		"Portugal",
-		"Romania",
-		"Slovakia",
-		"Slovenia",
-		"Spain",
-		"Sweden",
-		"United Kingdom"];
-	var query = startsWith(req.query.term);
-	res.json(memberState.filter( (el) => query.test(el)));
+}`, function (r) {
+        let json = JSON.parse(r);
+        let result = json.results.bindings.map(function (item) {
+            return {name: item.name.value, uri: item.artist.value};
+        });
+        res.json(result);
+    })
 });
 
 module.exports = router;
-
