6135e77ba6c171ed21f507ce779239f0db2ccee2
Mario Villani <villani.mario90@gmail.com>
2017-07-03 16:13:36 +0200

    select and new api

--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,74 @@
 let router = require('express').Router();
 let sparql = require('./sparql');
 
-router.get('/autocomplete/', function (req, res) {
+router.get('/autocomplete/artist', function (req, res) {
     if (!req.query.term)
         res.status(400).send('No query param');
-    sparql.query(`SELECT DISTINCT ?name ?artist ?bDate ?abstract ?image
+    sparql.query(`SELECT DISTINCT ?name ?artist
 WHERE
 {
   ?artist a umbelrc:MusicalPerformer;
     foaf:name ?name;
-  dbo:abstract ?abstract;
-  dbo:birthDate ?bDate.
- OPTIONAL{?artist dbo:thumbnail ?image}.
  FILTER regex(?name, "^${req.query.term}","i").
 }`, function (r) {
         let json = JSON.parse(r);
         let result = json.results.bindings.map(function (item) {
             return {name: item.name.value, uri: item.artist.value};
+        });
+        res.json(result);
+    })
+});
+
+router.get('/autocomplete/track', function (req, res) {
+    if (!req.query.term)
+        res.status(400).send('No query param');
+
+    sparql.query(`SELECT ?name ?track
+WHERE{
+  ?track foaf:name ?name;
+   {?track a umbelrc:MusicSingle} UNION {?track a dbo:MusicalWork}.
+     FILTER regex(?name, "^${req.query.term}","i").
+}`, function (r) {
+        let json = JSON.parse(r);
+        let result = json.results.bindings.map(function (item) {
+            return {name: item.name.value, uri: item.track.value};
+        });
+        res.json(result);
+    })
+});
+
+router.get('/autocomplete/band', function (req, res) {
+    if (!req.query.term)
+        res.status(400).send('No query param');
+    sparql.query(`SELECT ?name ?band
+WHERE 
+{
+	?band foaf:name ?name.
+    ?band a dbo:Band.
+     FILTER regex(?name, "^${req.query.term}","i").
+	}`, function (r) {
+        let json = JSON.parse(r);
+        let result = json.results.bindings.map(function (item) {
+            return {name: item.name.value, uri: item.band.value};
+        });
+        res.json(result);
+    })
+});
+
+router.get('/autocomplete/album', function (req, res) {
+    if (!req.query.term)
+        res.status(400).send('No query param');
+    sparql.query(`SELECT ?name ?album 
+WHERE 
+{
+	?album foaf:name ?name.
+	?album a schema:MusicAlbum.
+    FILTER regex(?name, "^${req.query.term}","i").
+}`, function (r) {
+        console.log(r);
+        let json = JSON.parse(r);
+        let result = json.results.bindings.map(function (item) {
+            return {name: item.name.value, uri: item.album.value};
         });
         res.json(result);
     })
--- routes/sparql.js
+++ routes/sparql.js
@@ -8,27 +8,29 @@ let endpoint = 'http://lodmusic.cloudapp.net:26109/lodmusic/sparql';
 let prefixes = `PREFIX umbelrc: <http://umbel.org/umbel/rc/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX dbp: <http://dbpedia.org/property/>`;
+PREFIX dbp: <http://dbpedia.org/property/>
+PREFIX schema: <http://schema.org/>`;
+
 
 let request = function (q, next) {
 
-	rp({
-		method: 'POST',
-		uri: endpoint,
-		form: {
-			query: prefixes + q
-		},
-		headers : {
-			Accept: 'application/sparql-results+json,*/*;q=0.9'
-		}
+    rp({
+        method: 'POST',
+        uri: endpoint,
+        form: {
+            query: prefixes + q
+        },
+        headers: {
+            Accept: 'application/sparql-results+json,*/*;q=0.9'
+        }
 
-})
-		.then(function (parsedBody) {
-			next(parsedBody);
-		})
-		.catch(function (err) {
-			next(err);
-		});
+    })
+        .then(function (parsedBody) {
+            next(parsedBody);
+        })
+        .catch(function (err) {
+            next(err);
+        });
 };
 
 module.exports.query = request;
